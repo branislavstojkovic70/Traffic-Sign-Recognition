f9a1ed40534af0f3cf2cda677caeb8d22dacf6b8
branislavstojkovic70 <96660978+branislavstojkovic70@users.noreply.github.com>
2024-01-30 12:51:59 +0100

    minor changes

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
-# Traffic Signs Recognition 
+# Traffic Signs Recognition
 
 ## Introduction
+
 This project focuses on the implementation of a Convolutional Neural Network (CNN) using Keras and TensorFlow to recognize and classify traffic signs. The model is trained on a dataset containing over 50,000 images of traffic signs and can classify these into 43 different categories. This technology has significant applications in developing autonomous vehicles and enhancing road safety.
 
 ## Dataset
-The dataset used is available on Kaggle and comprises more than 50,000 images of various traffic signs across 43 classes. The dataset is split into 'train' and 'test' folders, with each category having a range of images. The dataset can be downloaded from [Traffic Signs Dataset](<link-to-dataset>).
+
+The dataset used is available on Kaggle and comprises more than 50,000 images of various traffic signs across 43 classes. The dataset is split into 'train' and 'test' folders, with each category having a range of images. The dataset can be downloaded from [Traffic Signs Dataset](link-to-dataset).
 
 ## Prerequisites
+
 Before running the project, ensure you have the following installed:
+
 - Python 3
 - TensorFlow
 - Keras
@@ -18,33 +22,41 @@ Before running the project, ensure you have the following installed:
 - PIL (Python Imaging Library)
 
 You can install them using pip:
+
 ```bash
 pip install tensorflow keras numpy pandas matplotlib scikit-learn pillow
 ```
 
 ## Installation
+
 Clone the repository to your local machine:
+
 ```bash
 git clone <repository-url>
 ```
 
 ## Usage
+
 Navigate to the project directory and run the Python script:
+
 ```bash
 python traffic_signs_recognition.py
 ```
 
 ## Model Architecture
+
 The CNN model consists of multiple convolutional layers, max pooling layers, dropout layers, and dense layers for the final classification. The architecture is designed to effectively learn from the traffic sign images and classify them into one of the 43 categories.
 
 ## Training and Validation
+
 The model is trained on the training dataset and validated using a separate set of data. It achieves an accuracy of around 95% on the validation set.
 
 ## Testing
+
 The model is tested on a separate test dataset. The accuracy and performance of the model on this unseen data provide an indication of how well the model generalizes.
 
 ## GUI Application
+
 A Tkinter-based GUI application is included for easy interaction with the model. Users can upload traffic sign images, and the model will classify them in real-time.
 
-
-
+![Image showing chart with accuracy and epochs](accuracy5epochs.png)
--- model.py
+++ model.py
@@ -10,6 +10,7 @@ from keras.utils import to_categorical
 from keras.models import Sequential, load_model
 from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
 import matplotlib
+
 matplotlib.use('TkAgg')  # Or try 'Agg', 'Qt5Agg', etc.
 
 data = []
@@ -17,40 +18,36 @@ labels = []
 classes = 43
 cur_path = os.getcwd()
 
-#Retrieving the images and their labels
+# Retrieving the images and their labels
 for i in range(classes):
-    path = "archive/Train/"+str(i)
+    path = "archive/Train/" + str(i)
     images = os.listdir(path)
 
     for a in images:
         try:
-            image = Image.open(path + '//'+ a)
-            image = image.resize((30,30))
+            image = Image.open(path + '//' + a)
+            image = image.resize((30, 30))
             image = np.array(image)
-            #sim = Image.fromarray(image)
+            # sim = Image.fromarray(image)
             data.append(image)
             labels.append(i)
         except:
             print("Error loading image")
-#
-# #Converting lists into numpy arrays
+
+# Converting lists into numpy arrays
 data = np.array(data)
 labels = np.array(labels)
-#
 print(data.shape, labels.shape)
-# #Splitting training and testing dataset
+# Splitting training and testing dataset
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
-#
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-#
-# #Converting the labels into one hot encoding
+# Converting the labels into one hot encoding
 y_train = to_categorical(y_train, 43)
 y_test = to_categorical(y_test, 43)
-#
-# #Building the model
+# Building the model
 model = Sequential()
-model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=X_train.shape[1:]))
-model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu'))
+model.add(Conv2D(filters=32, kernel_size=(5, 5), activation='relu', input_shape=X_train.shape[1:]))
+model.add(Conv2D(filters=32, kernel_size=(5, 5), activation='relu'))
 model.add(MaxPool2D(pool_size=(2, 2)))
 model.add(Dropout(rate=0.25))
 model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
@@ -62,14 +59,14 @@ model.add(Dense(256, activation='relu'))
 model.add(Dropout(rate=0.5))
 model.add(Dense(43, activation='softmax'))
 
-#Compilation of the model
+# Compilation of the model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 epochs = 5
 history = model.fit(X_train, y_train, batch_size=32, epochs=epochs, validation_data=(X_test, y_test))
 # model.save("my_model.h5")
 
-#plotting graphs for accuracy
+# plotting graphs for accuracy
 plt.figure(0)
 plt.plot(history.history['accuracy'], label='training accuracy')
 plt.plot(history.history['val_accuracy'], label='val accuracy')
@@ -88,7 +85,7 @@ plt.ylabel('loss')
 plt.legend()
 plt.show()
 
-#testing accuracy on test dataset
+# testing accuracy on test dataset
 from sklearn.metrics import accuracy_score
 
 y_test = pd.read_csv('archive/Test.csv')
@@ -96,19 +93,19 @@ y_test = pd.read_csv('archive/Test.csv')
 labels = y_test["ClassId"].values
 imgs = y_test["Path"].values
 
-data=[]
+data = []
 
 for img in imgs:
-    image = Image.open("archive/Train/"+img)
-    image = image.resize((30,30))
+    image = Image.open("archive/Train/" + img)
+    image = image.resize((30, 30))
     data.append(np.array(image))
 
-X_test=np.array(data)
+X_test = np.array(data)
 
 pred_probabilities = model.predict(X_test)
 pred_classes = np.argmax(pred_probabilities, axis=1)
 
-
-#Accuracy with the test data
+# Accuracy with the test data
 from sklearn.metrics import accuracy_score
+
 print(accuracy_score(labels, pred_classes))
